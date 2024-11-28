c3b198fbd854551665ec47d61a40d080ef2230d4
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-11-28 10:48:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false => 0
+
+
+// let isLoggedin = 0
+
+// let valueInBoolean = Boolean(isLoggedin)
+// console.log(typeof valueInBoolean)
+// console.log(valueInBoolean)
+// 1 => "true"; 0 => false
+// "" => false
+// "Prashant" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
