a3391d8ea5d40fcb0e2e41baaf8da10d450325f8
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-12-04 05:16:15 +0000

    conversionOperations Done.

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,7 +21,39 @@ let score = "33"
 // "" => false
 // "Prashant" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************************************  Operations  *******************************************
+
+// console.log(2*3)
+
+// let str1 = "Prashant"
+// let str2 = "Anand"
+// let str3 = str1 + " " + str2
+// console.log(str3)
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+// console.log("1" + 2)
+// console.log(2 + "1")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+// console.log(num1)
+
+// let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter)
+
+
+
