c6133d89d896bf11ac728de1c1e236a54d30651a
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-12-04 09:57:35 +0000

    strings done.

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "Prashant"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value")// outdated
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)// Modern String Interposition(backticks)
+
+const gameName = new String("Prashant")
+
+// console.log(gameName[0])
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('h'))
+// console.log(gameName.length)
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
+
+// const anothernewString = "          Prashant            "
+// console.log(anothernewString)
+// console.log(anothernewString.trim())
+
+const url = "https://Prashant%20Anand"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('Prashant'))
+console.log(url.includes('hitesh'))
