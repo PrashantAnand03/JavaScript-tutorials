abaffb3cc8639dd5bfd6f1505fd66d85eab4a59e
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-12-04 11:49:00 +0000

    string interpolation update

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,7 +3,7 @@ const repoCount = 50
 
 // console.log(name + repoCount + "Value")// outdated
 
-console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)// Modern String Interposition(backticks)
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)// (Most Modern preferred JavaScript ES6) Called string interpolation uses(backticks)
 
 const gameName = new String("Prashant")
 
