3cca81ef8f1ae8497bdd04addd671b5ae43a38f6
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-12-04 06:56:10 +0000

    stack and heap memory done.

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,34 +2,58 @@
 
 // 7 types : String, Number, boolean, null, Undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId)
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
 
-const bigNumber = 12345678916273456769846n
+// const bigNumber = 12345678916273456769846n
 
 
 // Reference (Not primitive) -- call by reference(in it reference of memory address is passed and the changes made that occur  in address value)
 // Arrays, objects, Functions 
 
-const heros = ["shaktiman","naagraj","doga"];
+// const heros = ["shaktiman","naagraj","doga"];
 
 // inside curly braces we write key:value pair called object 
-let myObj = {
-    name: "Prashant",
-    age: "22",
+// let myObj = {
+//     name: "Prashant",
+//     age: "22",
+// }
+
+// const myFunction = function() {
+//     console.log("Hello World");
+// }
+
+
+// console.log(typeof myFunction);
+
+
+// ******************************** Memory Threads ********************************************
+
+// Stack --> primitive   Heap  --> reference
+
+
+// let myYoutubename = "PrashantAnanddotcom"
+// let anothername = myYoutubename
+// anothername = "chaiaurcode"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upiId: "user@ybl",
 }
+let userTwo = userOne
+userTwo.email = "prashant@google.com"
 
-const myFunction = function() {
-    console.log("Hello World");
-}
+console.log(userOne.email)
 
 
-console.log(typeof myFunction);
