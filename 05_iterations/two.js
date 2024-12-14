f37fec2463af9691f5560abc4842bb53e996c53d
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-12-14 19:33:22 +0000

    While loop, do while loop done.

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// While loop
+
+// let val = 0
+// while (val < 10) {
+//     console.log(`value of val is: ${val}`)
+//     val += 2
+// }
+
+// const myArray = ["flash","batman","superman"]
+// let idx = 0
+// while (idx < myArray.length) {
+//     console.log(`val is ${myArray[idx]}`)
+//     idx += 1
+// }
+
+// do while loop -- Condition check in the end
+
+let val = 11
+
+do {
+    console.log(`value is ${val}`)
+    val++
+} while (val <= 10);
